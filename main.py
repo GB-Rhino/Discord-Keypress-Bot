eef053010a19f32c1c5fb60c753b158b32133982
GB-Rhino <74067446+GB-Rhino@users.noreply.github.com>
2024-05-09 00:24:45 +0100

    Added focus functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import pydirectinput
 import logging
 import os
+import pygetwindow as gw
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -29,8 +30,16 @@ command_map = {
 
 
 }
-def handle_command(command):
+## TITLE NEEDS TO BE CHANGED TO THE TITLE OF GAME WINDOW
+def is_window_focused(window_title):
+    try:
+        window = gw.getWindowsWithTitle(window_title)[0]
+        return window.isActive
+    except IndexError:
+        print(f"No window found with the title '{window_title}'.")
+        return False
     
+def handle_command(command):
     if command in command_map:
         pydirectinput.press(command_map[command])
         logging.info(f"Executed command: {command}, Key: {command_map[command]}")
@@ -39,25 +48,19 @@ def handle_command(command):
     return "Command not recognized."
 
 @client.event
-async def on_ready():
-    print(f'Logged in as {client.user}')
-    print("Ready and listening...")
-
-@client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    
-    print(f"Received message: '{message.content}' from {message.author}")
-
-    
     if message.content.startswith('!cmd '):
-        command = message.content[5:].strip()  
-        response = handle_command(command)
-        await message.channel.send(response)
+        if is_window_focused('ELDEN RING™'):
+            command_text = message.content[5:].strip().lower()
+            response = handle_command(command_text)
+            await message.channel.send(response)
+        else:
+            await message.channel.send("The game window is not maximized. Please maximize the window to proceed.")
     else:
-        print("Message did not start with '!cmd '")
+        print(f"Message not starting with '!cmd ': {message.content}")
 
 
 token = os.getenv("DPG_DISCORD_TOKEN")
