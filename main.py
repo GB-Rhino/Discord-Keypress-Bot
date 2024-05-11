ab700369427f72d809365fadb82d19ac50856d02
GB-Rhino <74067446+GB-Rhino@users.noreply.github.com>
2024-05-11 13:24:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ async def on_message(message):
     
     
 
-    if message.content.startswith('!') and c:
+    if message.content.startswith('!'):
         command_text = message.content[1:].strip().lower()  # Extract the command after '!cmd '
         
         if is_valid_game_window(configurations):  # Check if the active window is a valid game
